82e461c8073874dd502b4e040bec83442d5b56bc
pir-anha <bogdan.czyz@gmail.com>
2020-03-01 21:17:16 +0100

    changed Notes to Notatki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
       icon: 'warning'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Family', 'Friends', 'Notatki', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
